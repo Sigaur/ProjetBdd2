e0fecc5a9798388173c57abe37a642884dd5a87b
Sigaur <marc.gayraud@efrei.net>
2017-05-28 14:27:48 +0200

    itineraire 1.0

--- itineraire.php
+++ itineraire.php
@@ -31,8 +31,62 @@
     	public $ville;
 	}
 
+	function displayResultats($close, $tempAutoroute, $villeDepart, $villeArrive)
+	{
+		$resultats = "$villeDepart";
+		$path = array();
+
+		array_push($path, $villeArrive);
+		array_push($path, $tempAutoroute);
+		$temp = $tempAutoroute;
+
+		/*
+		for($i = 0; $i < count($close); $i++)
+		{
+			$tempNode = clone $close[$i];
+			print("$tempNode->from -> $tempNode->ville -> $tempNode->autoroute <br>");
+		}
+		*/
+		
+
+		while($temp != 'start')
+		{
+			for($i = 0; $i < count($close); $i++)
+			{
+				$tempNode = clone $close[$i];
+				if($tempNode->autoroute == $temp)
+				{
+					array_push($path, $tempNode->ville);
+					array_push($path, $tempNode->from);
+					$temp = $tempNode->from;
+					$i = count($close);
+				}
+			}
+		}
+
+		for($i = count($path)-1; $i >= 0 ; $i--)
+		{
+			$temp = $path[$i];
+			if("$temp" != "start")
+			{
+				if($i != 0)
+				{
+					print("$temp->");
+				}
+				else
+				{
+					print("$temp");
+				}
+					
+			}
+			
+		}
+
+	}
+
 	function getItineraire($villeDepart, $villeArrive)
 	{
+		/*
 		print("Itineraire $villeDepart -> $villeArrive : <br><br>");
 
 		$tempNode = new Node();
@@ -132,9 +186,173 @@
 			}
 			$i ++;
 		}
+		*/
 
-		print("Pas d'itineraire trouves :");
+		print("Itineraire $villeDepart -> $villeArrive : <br><br>");
+
+		$tempNode = new Node();
+
+		$autorouteDepart = array();
+		$autorouteArrive = array();
+
+		$villes = array();
+
+		$open = array();
+		$close = array();
+
+		$results_id = query_database("SELECT CodA FROM troncons JOIN sorties ON troncons.CodT = sorties.CodT WHERE sorties.libelle = \"$villeDepart\"");
+
+		$i = 0;
 		
+		while ($row = $results_id->fetch_assoc())
+		{
+			array_push($autorouteDepart, $row['CodA']);
+			$tempNode->autoroute =  $row['CodA'];
+			$tempNode->ville = $villeDepart;
+			$tempNode->from = 'start';
+
+			array_push($open, $tempNode);
+			//array_push($close, $tempNode);
+			$i ++;
+		}
+
+		if(count($autorouteDepart) == 0)
+		{
+			print("La ville de depart n'est pas connectee au reseau d'autoroutes.");
+			die();
+		}
+
+		$results_id = query_database("SELECT CodA FROM troncons JOIN sorties ON troncons.CodT = sorties.CodT WHERE sorties.libelle = \"$villeArrive\"");
+		 print("<br>");
+		
+		$i = 0;
+		while ($row = $results_id->fetch_assoc())
+		{
+			array_push($autorouteArrive, $row['CodA']);
+			//print("$autorouteArrive[$i] <br>");			
+			$i ++;
+		}
+
+		if(count($autorouteArrive) == 0)
+		{
+			print("La ville d'arrivee n'est pas connectee au reseau d'autoroutes.");
+			die();
+		}
+
+		for($i = 0; $i < count($autorouteDepart); $i++)
+		{
+			for($j = 0; $j < count($autorouteArrive); $j++)
+			{
+				if($autorouteDepart[$i] == $autorouteArrive[$j])
+				{
+					print("$villeDepart -> $autorouteDepart[$i] -> $villeArrive");					
+					print("<br><br><a href='index.php'>Revenir au Menu Principal</a>");
+					die();
+				}
+			}
+		}
+
+		$compt = 0;
+		while($compt < count($open))
+		{
+			$tempNode = clone $open[$compt];
+			$from = $tempNode->autoroute;
+			
+			$results_id = query_database("SELECT sorties.libelle FROM sorties JOIN troncons ON sorties.CodT = troncons.CodT WHERE troncons.CodA = \"$from\"");
+			//print("<br>1 : $from<br>");
+			unset($villes);
+			$villes = array();
+
+			while ($row = $results_id->fetch_assoc())
+			{
+				$tempVille = $row['libelle'];
+				
+				if($tempVille != $from)
+				{					
+					array_push($villes, $tempVille);
+				}//print("2 : $tempVille<br>");
+			}
+
+			
+			for ($i = 0; $i < count($villes); $i++)
+			{
+				$tempVille = $villes[$i];
+				
+				$results_id = query_database("SELECT troncons.CodA FROM troncons JOIN sorties ON sorties.CodT = troncons.CodT WHERE sorties.libelle = \"$tempVille\"");
+				while ($row = $results_id->fetch_assoc())
+				{
+					$tempAutoroute = $row['CodA'];
+					//print("3 : $tempAutoroute<br>");
+					if($tempAutoroute != $from)
+					{
+						//Searching if $tempAutoroute is in close
+						$isIn = 0;
+						for ($j=0; $j < count($close); $j++) {
+							$tempNode = clone $close[$j];
+							if($tempAutoroute == $tempNode->autoroute)
+							{
+								$isIn = 1;
+							}
+						}
+						//Searching if $tempAutoroute is in open
+						for ($j=0; $j < count($open); $j++) {
+							$tempNode = clone $open[$j];
+							if($tempAutoroute == $tempNode->autoroute)
+							{
+								$isIn = 1;
+							}
+						}
+
+						if ($isIn == 0) 
+						{
+							//print("$tempAutoroute");
+							$tempNode->autoroute = $tempAutoroute;
+							$tempNode->ville = $tempVille;
+							$tempNode->from = $from;
+							array_push($open, $tempNode);							
+						}
+					}
+					
+				}
+			}
+
+
+			
+
+			$tempNode = clone $open[$compt];
+			array_push($close, $tempNode);
+
+			$temp2 = $tempNode->autoroute;
+
+			
+			if (in_array($temp2, $autorouteArrive)) 
+			{
+				//print("TEMPORAIRE : $temp2 <br>");
+				/*
+				for($i = 0; $i < count($close); $i++)
+				{
+					$tempNode = clone $close[$i];
+					print("$tempNode->from -> $tempNode->ville -> $tempNode->autoroute <br>");
+				}
+				*/
+				
+
+				displayResultats($close, $temp2, $villeDepart, $villeArrive);
+				die();
+			}
+			
+			$compt ++;
+			//unset($open[0]);
+			//array_values($open);
+		}
+		
+		print("Aucun chemins trouvés. Affichagte Débug des chemins trouvés par exploration :<br>");
+		for($i = 0; $i < count($close); $i++)
+		{
+			$tempNode = clone $close[$i];
+
+			print("$tempNode->from -> $tempNode->ville -> $tempNode->autoroute <br>");
+		}
 
 		/*
 		$i = 0;
